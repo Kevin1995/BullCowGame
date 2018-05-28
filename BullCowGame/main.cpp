ec644f028c97efee4c14ed965a98e6139ac38b90
Kevin1995 <kevinkehoe18@gmail.com>
2018-05-28 11:26:43 +0100

    getline and functions
    takes in strings with spaces in them
    
    starting to use funtions to manage complexity

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,23 +2,37 @@
 #include <string>
 
 using namespace std;
-int main() 
-{ 
+
+void PrintIntro() 
+{
 	// Introduce Game
 	// Constant Expression
 	constexpr int WORD_LENGTH = 5;
-
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+int main() 
+{ 
+	PrintIntro();
 
 	// get guest from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
-
+	getline(cin,Guess);
 	// repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
+
+	// get guest from the player
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 
 	return 0;
 }
