b93255b5754aa700aad7c4bb75b24dcd7a94f3f9
Kevin1995 <kevinkehoe18@gmail.com>
2018-05-28 11:16:08 +0100

    cin for user input
    A small interesting task to type in an input and trace it out on the screen

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main() 
 { 
+	// Introduce Game
 	// Constant Expression
 	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+
+	// get guest from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
 	return 0;
 }
