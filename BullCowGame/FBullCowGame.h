25f2c06f554e0ffdd20ab223626a09239c92b35b
Kevin1995 <kevinkehoe18@gmail.com>
2018-05-29 11:07:51 +0100

    GetHiddenWordLength
    We have a getter function which will get the length of the hidden word

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -18,11 +18,12 @@ public:
 
 	int32 GetMaxTries() const;
 	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+
+	bool CheckGuessValidity(FString) const; // TODO make a more rich return value.
 	bool IsGameWon() const;
 
 	void Reset(); // TODO make a more rich return value.
-	bool CheckGuessValidity(FString); // TODO make a more rich return value.
-
 	FBullCowCount SubmitGuess(FString);
 
 private:
